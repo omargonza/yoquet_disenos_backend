19bdf0252779ca7b5b1e00b9e6022a76ddbb7625
Gonzalo <gonzamartinez1081@gmail.com>
2025-11-24 22:32:16 -0300

    /users/

--- users/urls.py
+++ users/urls.py
@@ -1,25 +1,24 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-
 from .views import (
     RegisterView,
+    MeView,
     RequestPasswordResetView,
     PasswordResetConfirmView,
-    MeView,
 )
 
 urlpatterns = [
-    # Registro
+    # Registro / Login / Refresh / Me
     path("register/", RegisterView.as_view(), name="register"),
-
-    # JWT
-    path("login/", TokenObtainPairView.as_view(), name="login"),
+    path("login/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("refresh/", TokenRefreshView.as_view(), name="token_refresh"),
-
-    # Password reset
-    path("password-reset/", RequestPasswordResetView.as_view(), name="password_reset"),
-    path("password-reset-confirm/", PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
-
-    # Usuario actual
     path("me/", MeView.as_view(), name="me"),
+
+    # Reset Password
+    path("password-reset/", RequestPasswordResetView.as_view(), name="password_reset"),
+    path(
+        "password-reset-confirm/",
+        PasswordResetConfirmView.as_view(),
+        name="password_reset_confirm",
+    ),
 ]
